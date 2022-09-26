defee3ec2ca7c577edf6e21895aa76694e87b923
LuteJr <101115777+LuteJr@users.noreply.github.com>
2022-09-26 20:29:51 +0300

    first commit

---
+++ pages/contact.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const contactPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact Page</title>
+      </Head>
+
+      <main>hello from nextpm</main>
+    </>
+  );
+};
+
+// este obligatoriu sa returnam un default export
+//pentru fiecare pagina
+
+export default contactPage;
