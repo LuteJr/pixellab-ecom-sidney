271da24080cad740ec0048a402c95b9833986414
LuteJr <101115777+LuteJr@users.noreply.github.com>
2022-09-26 21:47:20 +0300

    2147

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <main className="text-3l font-bold underline">homepage</main>
+    </>
+  );
+}
